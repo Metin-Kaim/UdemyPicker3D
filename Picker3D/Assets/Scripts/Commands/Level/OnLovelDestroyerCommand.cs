4373f2148c0587afff7ccb8a51f57a7e0f43ccd4
Metin Kaim <metinkaim0@gmail.com>
2023-09-06 19:18:00 +0300

    Level System Finalized

---
+++ Picker3D/Assets/Scripts/Commands/Level/OnLovelDestroyerCommand.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+namespace Commands.Level
+{
+    public class OnLovelDestroyerCommand
+    {
+        private Transform _levelHolder;
+
+        public OnLovelDestroyerCommand(Transform levelHolder)
+        {
+            _levelHolder = levelHolder;
+        }
+
+        public void Execute()
+        {
+            if (_levelHolder.transform.childCount <= 0) return;
+            Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
+        }
+    }
+}
---
+++ Picker3D/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,113 @@
+﻿using Commands.Level;
+using Data.UnityObjects;
+using Data.ValueObjects;
+using Signals;
+using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private Transform levelHolder;
+        [SerializeField] private byte totalLevelCount;
+
+        #endregion
+
+        #region Private Variables
+
+        private OnLevelLoaderCommand _levelLoaderCommand;
+        private OnLovelDestroyerCommand _levelDestroyerCommand;
+
+
+        private short _currentLevel;
+        private LevelData _levelData;
+
+
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            _levelData = GetLevelData();
+            _currentLevel = GetActiveLevel();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            _levelLoaderCommand = new(levelHolder);
+            _levelDestroyerCommand = new(levelHolder);
+        }
+
+        private LevelData GetLevelData()
+        {
+            return Resources.Load<CD_Level>("Data/CD_Level").Levels[_currentLevel];
+        }
+
+        private byte GetActiveLevel()
+        {
+            return (byte)_currentLevel;
+        }
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize += _levelLoaderCommand.Execute;
+            CoreGameSignals.Instance.onClearActiveLevel += _levelDestroyerCommand.Execute;
+            CoreGameSignals.Instance.onGetLevelValue += OnGetLevelValue;
+            CoreGameSignals.Instance.onNextLevel += OnNextLevel;
+            CoreGameSignals.Instance.onRestartLevel += OnRestartLevel;
+        }
+
+        private byte OnGetLevelValue()
+        {
+            return (byte)_currentLevel;
+        }
+        private void OnNextLevel()
+        {
+            _currentLevel++;
+            CoreGameSignals.Instance.onClearActiveLevel();
+            CoreGameSignals.Instance.onReset?.Invoke();
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount));
+        }
+
+        private void OnRestartLevel()
+        {
+            CoreGameSignals.Instance.onClearActiveLevel();
+            CoreGameSignals.Instance.onReset?.Invoke();
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount));
+        }
+
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize -= _levelLoaderCommand.Execute;
+            CoreGameSignals.Instance.onClearActiveLevel -= _levelDestroyerCommand.Execute;
+            CoreGameSignals.Instance.onGetLevelValue -= OnGetLevelValue;
+            CoreGameSignals.Instance.onNextLevel -= OnNextLevel;
+            CoreGameSignals.Instance.onRestartLevel -= OnRestartLevel;
+        }
+
+        private void Start()
+        {
+            CoreGameSignals.Instance.onLevelInitialize?.Invoke((byte)(_currentLevel % totalLevelCount));
+            //UISignals
+        }
+
+        
+    }
+}
